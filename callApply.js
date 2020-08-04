a77456208bdcf306258fdbb60dee5202d28ac0c9
as-real-me <real.uiucse@gmail.com>
2020-08-04 07:28:39 +0600

    Bind

--- callApply.js
+++ callApply.js
@@ -11,6 +11,26 @@ const normalPerson = {
         return this.salary;
     }
 }
-console.log(normalPerson.getFullName());
-console.log(normalPerson.salary);
-console.log(normalPerson.chargeBill(1000));
+
+
+const heroPerson = {
+    firstName: "Hero",
+    lastName: "Kamal",
+    salary: 25000,
+}
+
+const heroFriend = {
+    firstName: "Hero",
+    lastName: "Jamal",
+    salary: 25000,
+}
+
+// Binding normalPerson's chargeBill function with heroPerson
+console.log(heroPerson.salary);
+const heroBillCharge = normalPerson.chargeBill.bind(heroPerson);
+heroBillCharge(3000);
+console.log(heroPerson.salary);
+
+// Binding normalPerson's getFullName function with herofriend
+const heroFriendFullName = normalPerson.getFullName.bind(heroFriend);
+console.log(heroFriendFullName());
